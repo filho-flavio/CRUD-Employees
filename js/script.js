a2aca67a44abd6b2e71f63670ab97769444270a0
Flavio <flaviofilholm10@gmail.com>
2023-10-18 12:15:17 -0300

    update code

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ document
   .addEventListener("click", toggleFormVisibility);
 
 let content = {
+  //inId: undefined,
   inName: document.querySelector("#inName"),
   inPosition: document.querySelector("#inPosition"),
   inOffice: document.querySelector("#inOffice"),
@@ -40,36 +41,116 @@ function validateForm() {
       return;
     }
   }
-  addEmployee(content);
-}
-
-function addEmployee(data) {
-  let table = document.querySelector("table");
-  const row = table.insertRow();
-
-  for (let num in data) {
-    const cell = row.insertCell();
-    cell.textContent = data[num].value;
-  }
-
-  table.appendChild(row);
-  const actionsCell = row.insertCell(); // Crie uma célula para os botões de ação.
-  const editButton = document.createElement("button");
-  editButton.textContent = "Edit";
-  editButton.className = "btEdit";
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete";
-  deleteButton.className = "btDelete";
-  actionsCell.appendChild(editButton);
-  actionsCell.appendChild(deleteButton);
-  actionsCell.className = "btActions";
-
-  clearDisplay();
-  document.querySelector("#div-add").classList.toggle("hidden");
+  createEmployee(content);
+  readEmployee();
 }
 
 function clearDisplay() {
   for (let num in content) {
     content[num].value = "";
+  }
+}
+
+function createEmployee(employee) {
+  let storedList = localStorage.getItem("dbClient");
+  let parsedList = storedList ? JSON.parse(storedList) : [];
+
+  let dataEmployees = {
+    name: employee.inName.value,
+    position: employee.inPosition.value,
+    office: employee.inOffice.value,
+    age: employee.inAge.value,
+    startDate: employee.inStartDate.value,
+    telephone: employee.inTelephone.value,
+    salary: employee.inSalary.value,
+  };
+
+  parsedList.push(dataEmployees);
+
+  localStorage.setItem("dbClient", JSON.stringify(parsedList));
+
+  clearDisplay();
+  toggleFormVisibility();
+}
+
+function readEmployee() {
+  let data = JSON.parse(localStorage.getItem("dbClient"));
+  //console.log(data);
+
+  data.forEach((employee) => {
+    let table = document.querySelector("table");
+    const row = table.insertRow();
+
+    // Você precisa percorrer as propriedades do objeto `employee`
+    for (let key in employee) {
+      const cell = row.insertCell();
+      cell.textContent = employee[key]; // Use a chave (`key`) para acessar o valor correto no objeto `employee`
+    }
+
+    const actionsCell = row.insertCell();
+    const editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.className = "btEdit";
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "btDelete";
+    actionsCell.appendChild(editButton);
+    actionsCell.appendChild(deleteButton);
+    actionsCell.className = "btActions";
+  });
+}
+
+readEmployee();
+
+let btEdit = document.querySelectorAll(".btEdit");
+btEdit.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    updateEmployee(index);
+  });
+});
+
+function updateEmployee(index) {
+  let storedList = localStorage.getItem("dbClient");
+  let parsedList = storedList ? JSON.parse(storedList) : [];
+  let selectedEmployee;
+
+  if (index >= 0 && index < parsedList.length) {
+    selectedEmployee = parsedList[index];
+  }
+
+  fillForm(selectedEmployee);
+}
+
+function fillForm(employee) {
+  let content = {
+    inName: document.querySelector("#inName"),
+    inPosition: document.querySelector("#inPosition"),
+    inOffice: document.querySelector("#inOffice"),
+    inAge: document.querySelector("#inAge"),
+    inStartDate: document.querySelector("#inStartDate"),
+    inTelephone: document.querySelector("#inTelephone"),
+    inSalary: document.querySelector("#inSalary"),
+  };
+
+  content.inName.value = employee.name;
+  content.inPosition.value = employee.position;
+  content.inOffice.value = employee.office;
+  content.inAge.value = employee.age;
+  content.inStartDate.value = employee.startDate;
+  content.inTelephone.value = employee.telephone;
+  content.inSalary.value = employee.salary;
+
+  formVisibility();
+}
+
+function formVisibility() {
+  const divAdd = document.querySelector("#div-add");
+  divAdd.classList.toggle("hidden");
+  divAdd.classList.toggle("content");
+
+  if (divAdd.classList.contains("content")) {
+    btEdit.disabled = true;
+  } else {
+    btEdit.disabled = false;
   }
 }
